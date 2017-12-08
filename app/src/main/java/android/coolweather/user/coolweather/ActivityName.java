a0925001c2653f9942ebd247e1d2c5341303a945
lxblessyou <461795212@qq.com>
2017-12-08 17:27:54 +0800

    First commit.

---
+++ app/src/main/java/android/coolweather/user/coolweather/ActivityName.java
@@ -0,0 +1,13 @@
+package android.coolweather.user.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+    }
+}
